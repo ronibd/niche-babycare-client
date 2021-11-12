b2710deb50f184f547b08c541ab38380a368e2ee
Abu Noman Roni <abunomanroni@gmail.com>
2021-11-12 22:50:53 +0600

    updated

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,10 +1,10 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyA9ujKKC9XPIBBEffIkPN84TFltsySyW3I",
-  authDomain: "ema-zohn.firebaseapp.com",
-  projectId: "ema-zohn",
-  storageBucket: "ema-zohn.appspot.com",
-  messagingSenderId: "549750706653",
-  appId: "1:549750706653:web:b0c99a583b4d449ffe6634",
+  apiKey: "AIzaSyBd3naKeuKoa9BiiOju9U_JRgLg8wxIB8w",
+  authDomain: "babycare-397d3.firebaseapp.com",
+  projectId: "babycare-397d3",
+  storageBucket: "babycare-397d3.appspot.com",
+  messagingSenderId: "543204381237",
+  appId: "1:543204381237:web:4328d055c44f01feacd77c",
+  measurementId: "G-CT76ZF8RS6"
 };
-
-export default firebaseConfig;
+export default firebaseConfig
